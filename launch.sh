7b033f1342b8f2c48c4c0cb906f71329e3d0874a
Hadrien Croubois <hc@iex.ec>
2019-04-12 11:07:42 +0200

    remove clerk entry from launch.sh

--- launch.sh
+++ launch.sh
@@ -2,7 +2,6 @@
 
 
 python3 python/daemon.py                                      \
-	--clerk 0x8BE59dA9Bf70e75Aa56bF29A3e55d22e882F91bA          \
 	--hub 0x7C788C2B85E20B4Fa25bd579A6B1D0218D86BDd1            \
 	--contracts contracts \
 	$@

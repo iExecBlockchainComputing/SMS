9b0a52ff4608cfbd1fa24ac58f265c65e878b735
charles de mauroy <charles.demauroy@iex.ec>
2019-06-25 11:08:30 +0200

    final clean-up

--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 sms/daemon.py                                      \
+python3 python/daemon.py                                      \
 	--hub 0xb3901d04CF645747b99DBbe8f2eE9cb41A89CeBF            \
 	--contracts sms/node_modules/iexec-poco/build/contracts         \
 	--test

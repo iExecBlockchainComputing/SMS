e41609799cd353ab4a33ed13503825137c039629
charles de mauroy <charles.demauroy@iex.ec>
2019-06-26 14:53:41 +0200

    modifs for sms fusion

--- launch.sh
+++ launch.sh
@@ -3,5 +3,4 @@
 python3 python/daemon.py                                      \
 	--hub 0xb3901d04CF645747b99DBbe8f2eE9cb41A89CeBF            \
 	--contracts sms/node_modules/iexec-poco/build/contracts         \
-	--test
 	$@

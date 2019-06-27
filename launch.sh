5209e5c21359e58f56e4c473da17eeccc838c874
charles de mauroy <charles.demauroy@iex.ec>
2019-06-27 12:00:36 +0200

    add libscone-cli

--- launch.sh
+++ launch.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 python3 python/daemon.py                                      \
-	--hub 0xb3901d04CF645747b99DBbe8f2eE9cb41A89CeBF            \
-	--contracts sms/node_modules/iexec-poco/build/contracts         \
-	--casAddress 127.0.0.1
+	--contracts /node_modules/iexec-poco/build/contracts         \
 	$@

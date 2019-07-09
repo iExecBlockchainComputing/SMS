e22e0a239d3050e5d83206dee47bb21101d67416
charles de mauroy <charles.demauroy@iex.ec>
2019-07-09 11:25:37 +0200

    adding persistent db in launch conf

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,5 +4,6 @@ python3 /python/daemon.py \
           --hub $HUB \
           --gateway $GATEWAY \
           --casAddress $CAS \
+          --database $DATABASE \
           --contracts /node_modules/iexec-poco/build/contracts
           $@

9d704f82556d75eb792b5eb9967e9b69fd7cd619
charles de mauroy <charles.demauroy@iex.ec>
2019-07-10 14:06:27 +0200

    adding persistent db

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,6 @@ python3 /python/daemon.py \
           --hub $HUB \
           --gateway $GATEWAY \
           --casAddress $CAS \
-          --database $DATABASE \
+          --database sqlite:///sms/sms.db \
           --contracts /node_modules/iexec-poco/build/contracts
           $@
